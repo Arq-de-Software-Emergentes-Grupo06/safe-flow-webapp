739b071a9e4a72c2f48593fc69bb51c07647fa74
Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
2024-11-15 19:33:39 -0500

    feat/ added all contexts and feats
    Co-Authored-By: Alarsandro <116916774+Alarsandro@users.noreply.github.com>
    Co-Authored-By: Paolo Espejo <103698198+Loleao@users.noreply.github.com>
    Co-Authored-By: Rodrigo Pozo <rodrigo.pozo25@gmail.com>
    Co-Authored-By: Fabrizzio Laguerre <104078801+UwUInspector@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiciosComponent } from './Management/servicios/servicios.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { DeliveriesComponent } from './Management/deliveries/deliveries.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { IncidentsComponent } from './Incidents/incidents/incidents.component';
+import { MonitoringComponent } from './Management/monitoring/monitoring/monitoring.component';
+import { ComponentsComponent } from './Profiles/components/components.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiciosComponent,
+    DeliveriesComponent,
+    ToolbarComponent,
+    IncidentsComponent,
+    MonitoringComponent,
+    ComponentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
